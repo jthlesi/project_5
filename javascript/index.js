011b45451505ce224c24636da8c802069facc916
jthlesi <distanc87@gmail.com>
2022-07-18 13:00:35 +0900

    0718

--- javascript/index.js
+++ javascript/index.js
@@ -8,38 +8,50 @@ $(document).ready(function(){
 
 //슬라이드
 $(document).ready(function(){
-    $(".slide").not(":first").css({"width":"48px"})
+    $(".slide").not(":first").css({"width":"48px"});
+    $(".pageWrap").eq(0).show();
+    $(".bannerTxt").eq(0).show();
 })
 
 function prev() {
-    $(".slide:last").prependTo("#slideWrap");
-    $(".slide:first").stop().animate({"width":1024},"slow");
-    $(".slide").eq(1).stop().animate({"width":48},"slow");
+    $(".slide:last").prependTo("#slideWrap").css({"width":0});
+    $(".slide:first").stop().animate({"width":1024},"slow","linear");
+    $(".slide").eq(1).stop().animate({"width":48},"slow","linear");
+    $(".bannerTxt").eq(1).fadeOut("slow");
+    $(".bannerTxt").eq(0).fadeIn("slow");
+    $(".pageWrap").eq(1).fadeOut("slow");
+    $(".pageWrap").eq(0).fadeIn("slow");
 }
 
 function next(){
-    $(".slide:first").stop().animate({"width":0},"slow");
-    $(".slide").eq(1).stop().animate({"width":1024},"slow",function(){
+    $(".slide:first").stop().animate({"width":0},"slow","linear");
+    $(".slide").eq(1).stop().animate({"width":1024},"slow","linear",function(){
         $(".slide:first").appendTo("#slideWrap");
         $(".slide:last").css({"width":"48px"});
     });
+    $(".bannerTxt").eq(0).fadeOut("slow");
+    $(".bannerTxt").eq(1).fadeIn("slow");
+    $(".pageWrap").eq(0).fadeOut("slow");
+    $(".pageWrap").eq(1).fadeIn("slow");
 }
 
-var time = setInterval(next, 2000);
+// var time = setInterval(next, 4000);
+
+// $("#slides ul").hover(function(){
+//     clearInterval(time);
+// }, function(){
+//     time = setInterval(next, 4000);
+// })
 
 $("#prev").on("click", function(){
-    clearInterval(time);
     prev();
-    setTimeout(function(){
-        time = setInterval(next, 2000);
-    },1000);
 })
 
 $("#next").on("click", function(){
-    clearInterval(time);
     next();
-    setTimeout(function(){
-        time = setInterval(next, 2000);
-    },1000);
 })
+
+for(i=1; i<11; i++){
+    $("#slide_"+i).css("background","url(img/banner_"+i+".webp)")
+}
 
