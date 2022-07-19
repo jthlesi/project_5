9f19313ef9bdd0660c5dd22ce62fefb2bd4ac5c0
jthlesi <distanc87@gmail.com>
2022-07-19 13:17:09 +0900

    0719

--- javascript/index.js
+++ javascript/index.js
@@ -12,6 +12,8 @@ $(document).ready(function(){
     $(".pageWrap").eq(0).show();
     $(".bannerTxt").eq(0).show();
     $("#prev_2").addClass("hide");
+    $("#prev_3").addClass("hide");
+    $("#prev_4").addClass("hide");
 })
 
 function prev() {
@@ -74,45 +76,158 @@ $.ajax({
         }
     });
 
-
-
 var index = 0;
 
-function next_2(index) {
+function slide(index) {
     $("#slidesWrap_2").stop().animate({"marginLeft": - 1134 * (index) + "px"},"slow","linear");
 }
 
-function next_2_1(index) {
+function slide_1(index) {
     $("#slidesWrap_2").stop().animate({"marginLeft": - ((1134/3) + (1134 * (index))) + "px"},"slow","linear");
 }
 
-function next_2_2(index) {
+function slide_1_2(index) {
     $("#slidesWrap_2").stop().animate({"marginLeft": - ((1134 * (index))-((1134/3)*2)) + "px"},"slow","linear");
 }
 
-$("#next_2").click(function () {
-    if (index < 1) {
-        next_2(index + 1);
-        index++;
-        $("#prev_2").removeClass("hide");
-    } else if (index==1){
-        next_2_1(index);
-        index++;
-        $("#next_2").addClass("hide");
-    }
-})
-
 $("#prev_2").click(function () {
     if (index==2){
-        next_2_2(index-1)
+        slide_1_2(index-1)
         index--;
         $("#next_2").removeClass("hide");
     } else if (index == 1) {
-        next_2(index - 1);
+        slide(index - 1);
         index--;
         $("#next_2").removeClass("hide");
         $("#prev_2").addClass("hide");
     }
 })
 
+$("#next_2").click(function () {
+    if (index < 1) {
+        slide(index + 1);
+        index++;
+        $("#prev_2").removeClass("hide");
+    } else if (index==1){
+        slide_1(index);
+        index++;
+        $("#prev_2").removeClass("hide");
+        $("#next_2").addClass("hide");
+    }
+})
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "베스트셀러", size: 18},
+    headers: { Authorization: "KakaoAK ddf7da74924187d0c803e6910e6b67bc" }
+})
+
+    .done(function (msg) {
+        for (var i=0;i<19;i++){
+        $(".cover").eq(i).append("<img src="+msg.documents[i].thumbnail+">");
+        $(".title").eq(i).append("<p>"+msg.documents[i].title.substring(0,10)+"</p>");
+        $(".writer").eq(i).append("<p>"+msg.documents[i].authors.slice(0,2)+"</p>");
+        $(".rank").eq(i).append("<p>"+(i+1)+"</p>");
+        var title=msg.documents[i].title;
+        var author=msg.documents[i].authors;
+        if(title.length>10){
+            $(".title p").eq(i).append("...");
+        }
+        if(author.length>3) {
+            $(".writer p").eq(i).append(" 외");
+        }
+        }
+    });
+
+var index_2 =0;
+
+function slide_2(index_2){
+    $("#slidesWrap_3").stop().animate({"marginLeft": - 1134 * (index_2) + "px"},"slow","linear");
+};
+
+$("#next_3").click(function () {
+    if (index_2==0) {
+        slide_2(index_2 + 1);
+        index_2++;
+        $("#prev_3").removeClass("hide");
+        $("#next_3").addClass("hide");
+    }
+});
+
+$("#prev_3").click(function () {
+    if (index_2==1){
+        slide_2(index_2-1)
+        index_2--;
+        $("#next_3").removeClass("hide");
+        $("#prev_3").addClass("hide");
+    }
+});
+
+$(document).ready(function(){
+    $(".star").prepend("<i class='fa-solid fa-star'></i>")
+});
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "시작", size: 20},
+    headers: { Authorization: "KakaoAK ddf7da74924187d0c803e6910e6b67bc" }
+})
+
+    .done(function (msg) {
+        for (var i=0;i<21;i++){
+        $(".thumb_4").eq(i).append("<img src="+msg.documents[i].thumbnail+">");
+        $(".title_4").eq(i).append("<p>"+msg.documents[i].title.substring(0,12)+"</p>");
+        $(".author_4").eq(i).append("<p>"+msg.documents[i].authors.slice(0,2)+"</p>");
+        var title=msg.documents[i].title;
+        var author=msg.documents[i].authors;
+        if(title.length>10){
+            $(".title_4 p").eq(i).append("...");
+        }
+        if(author.length>3) {
+            $(".author_4 p").eq(i).append(" 외");
+        }
+        }
+    });
+
+var index_4 =0;
+var slideWidth = document.querySelector(".slide_4").offsetWidth
+var slideNum =5;
+var last=0
+
+function slide_4(index_4){
+    $("#slidesWrap_4").stop().animate({"marginLeft": - ((slideWidth * slideNum)+(slideWidth*last)) * (index_4)  + "px"},"slow","linear");
+};
+
+$("#next_4").click(function () {
+    if(index_4==2){
+        $("#next_4").addClass("hide");
+        last= 2;
+        slide_4(index_4);
+        index_4++;
+        last=0;
+    } else if (index_4>=0) {
+        slide_4(index_4 + 1);
+        index_4++;
+        $("#prev_4").removeClass("hide");
+    } 
+});
+
+$("#prev_4").click(function () {
+    if(index_4==1){   
+        slide_4(index_4-1);
+        index_4--;     
+        $("#prev_4").addClass("hide");
+    } else if(index_4<3){
+        slide_4(index_4-1);
+        index_4--;
+    } else if (index_4==3){
+        last=4;
+        slide_4(index_4-2);
+        index_4--;
+        last=0;
+        $("#next_4").removeClass("hide");
+    }
+});
 
